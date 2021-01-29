d5de711c9d473af26abb9fc7849363ebebda0d02
Vyacheslav Konovalov <crabvk@protonmail.com>
2021-01-29 09:39:11 +0500

    Check multiple files with check-transactions.fish

--- check-transactions.fish
+++ check-transactions.fish
@@ -1,27 +1,32 @@
 #!/usr/bin/env fish
 
-set transactions (cat $argv[1])
 set count_success 0
 set count_rejected 0
+set total 0
 
-for txid in $transactions
-    echo -n $txid'... '
-    set txst (
-        concordium-client transaction status $txid --grpc-ip 127.0.0.1 |\
-            rg -or '$1' '^Transaction is finalized into block .+ with status "(.+)".*'
-    )
+for file in $argv
+    set transactions (cat $file)
 
-    switch $txst
-        case success
-            set count_success (math "$count_success+1")
-        case rejected
-            set count_rejected (math "$count_rejected+1")
+    for txid in $transactions
+        echo -n $txid' ... '
+        set txst (
+            concordium-client transaction status $txid --grpc-ip 127.0.0.1 |\
+                rg -or '$1' '^Transaction is finalized into block .+ with status "(.+)".*'
+        )
+
+        switch $txst
+            case success
+                set count_success (math "$count_success+1")
+            case rejected
+                set count_rejected (math "$count_rejected+1")
+        end
+
+        set total (math "$total+1")
+        echo $txst
     end
-
-    echo $txst
 end
 
 echo
-echo 'Total transactions: '(count $transactions)
+echo 'Total transactions: '$total
 echo '           Succeed: '$count_success
 echo '          Rejected: '$count_rejected
